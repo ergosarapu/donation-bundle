65dc55c817393deeba36661eaeb4a3f5a51b1ddb
Ergo Sarapu <ergosarapu@gmail.com>
2024-05-13 14:46:13 +0000

    Include flowbite form theme

--- src/Controller/AbstractDonationController.php
+++ src/Controller/AbstractDonationController.php
@@ -4,17 +4,14 @@ namespace ErgoSarapu\DonationBundle\Controller;
 
 use ErgoSarapu\DonationBundle\Entity\Payment;
 use ErgoSarapu\DonationBundle\Form\DonationType;
-use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
-class DonationController extends AbstractController
+abstract class AbstractDonationController extends AbstractController
 {
 
-    #[Route('/donate', name: 'donate')]
-    public function new(Request $request): Response
+    public function handle(Request $request): Response
     {
         $payment = new Payment();
 
@@ -29,9 +26,11 @@ class DonationController extends AbstractController
             return $this->redirectToRoute('task_success');
         }
 
-        return $this->render('@DonationBundle/donation/_donate_form.html.twig', [
+        return $this->render($this->getTemplate(), [
             'form' => $form,
             'payment' => $payment
         ]);
     }
+
+    public abstract function getTemplate():string;
 }
