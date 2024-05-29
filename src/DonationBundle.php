8f21de4ae8397d5b19c8a9f714cf56ffe65373ed
Ergo Sarapu <ergosarapu@gmail.com>
2024-05-29 11:45:10 +0000

    Add payum status extension

--- src/DonationBundle.php
+++ src/DonationBundle.php
@@ -3,6 +3,7 @@
 namespace ErgoSarapu\DonationBundle;
 
 use ErgoSarapu\DonationBundle\Controller\IndexController;
+use ErgoSarapu\DonationBundle\Payum\UpdatePaymentStatusExtension;
 use ErgoSarapu\DonationBundle\Twig\Components\DonationForm;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -34,6 +35,8 @@ class DonationBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(__DIR__ . '/../config/controller.yaml');
+
+        $container->services()->set(UpdatePaymentStatusExtension::class, class: UpdatePaymentStatusExtension::class)->public()->tag('payum.extension', ['all' => true]);
         $container->services()->get(IndexController::class)->call('setPaymentMethods', [$config['payment_methods']]);
         $container->services()->get(DonationForm::class)->call('setPaymentMethods', [$config['payment_methods']]);
     }
