d5209e87f913f6769759e549939c5526783a890c
Ergo Sarapu <ergosarapu@gmail.com>
2024-09-25 05:24:18 +0000

    Remove debug dsn and set env variables for all steps

--- cli-config.php
+++ cli-config.php
@@ -24,8 +24,6 @@ $ORMConfig->setProxyNamespace('Proxies');
 $ORMConfig->setMetadataDriverImpl($driver);
 $ORMConfig->setNamingStrategy(new UnderscoreNamingStrategy());
 
-$dsn = getenv('DATABASE_URL');
-var_dump($dsn);
 if (!getenv('DATABASE_URL')) {
     throw new InvalidArgumentException('DATABASE_URL not available as environment variable');
 }
